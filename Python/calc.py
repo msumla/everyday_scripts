25dd697ed539bae4e4859a232188619ea917d28c
Margus Sumla <msumla@itcollege.ee>
2017-04-10 18:42:05 +0300

    Improvements

--- Python/calc.py
+++ Python/calc.py
@@ -1,4 +1,4 @@
-import sys
+import sys, math
 # Autor: Margus Sumla
 # 2017
 
@@ -22,12 +22,21 @@ def mul(a, b):
 def div(a, b):
     return a / b
 
+def pow(a, b):
+    return a ** b
+    
+def sqr(a):
+    return math.sqrt(a)
+
 def ans(c, e):
     print("%s %s %s = %.2f" % (a, c, b, e(a, b)))
+
+def ans_(c, e):
+    print("Ruutjuur %s-st = %.2f" % (a, e(a)))
 # kasutusjuhend
 if len(sys.argv) == 2 and sys.argv[1] == 'help':
     print('Kalkulaatori kasutamine:\n'
-          '\t1) kasuparameetritega  : sisesta tehte liikmed skripti kaivitamise kasu jarel tyhikutega eraldatult (1.operand [+, -, *, /] 2.operand). Naiteks: python3 calc.py 1 / 200\n'
+          '\t1) kasuparameetritega  : sisesta tehte liikmed skripti kaivitamise kasu jarel tyhikutega eraldatult (1.operand [+, -, *, /, ^] 2.operand). Naiteks: python3 calc.py 1 / 200\n'
           '\t2) kasitsi sisestusega : sisesta parameetrid ykshaaval vastavalt kysitule.')
 
     exit()
@@ -39,6 +48,15 @@ if len(sys.argv) == 4:
         a = float(sys.argv[1])
         c = str(sys.argv[2])
         b = float(sys.argv[3])
+    except:
+        print('Vale sisestus!')
+        print('Kasutusjuhendi nagemiseks lisa skripti kaivitamise kasu loppu sona help.')
+elif len(sys.argv) == 3:
+    print('Kasutusjuhendi nagemiseks lisa skripti kaivitamise kasu loppu sona help.\n')
+
+    try:
+        a = float(sys.argv[1])
+        c = str(sys.argv[2])
     except:
         print('Vale sisestus!')
         print('Kasutusjuhendi nagemiseks lisa skripti kaivitamise kasu loppu sona help.')
@@ -57,17 +75,17 @@ else:
         print('Tehe: ')
         c = str(inp())
     except:
-        print('Vale sisestus! Kasutada saab +, -, * voi /.')
+        print('Vale sisestus! Kasutada saab +, -, *, / voi ^.')
         print('Kasutusjuhendi nagemiseks lisa skripti kaivitamise kasu loppu sona help.\n')
         exit()
-
-    try:
-        print('Teine operand: ')
-        b = float(inp())
-    except:
-        print('Vale sisestus! Kasutada saab taisarve voi komakohaga arve (koma asemel on punkt).')
-        print('Kasutusjuhendi nagemiseks lisa skripti kaivitamise kasu loppu sona help.\n')
-        exit()
+    if c != '_':
+        try:
+            print('Teine operand: ')
+            b = float(inp())
+        except:
+            print('Vale sisestus! Kasutada saab taisarve voi komakohaga arve (koma asemel on punkt).')
+            print('Kasutusjuhendi nagemiseks lisa skripti kaivitamise kasu loppu sona help.\n')
+            exit()
 # tehted
 if c == '+':
     ans(c, add)
@@ -77,6 +95,10 @@ elif c == '*':
     ans(c, mul)
 elif c == '/':
     ans(c, div)
+elif c == '^':
+    ans(c, pow)
+elif c == '_':
+    ans_(c, sqr)
 else:
-    print('Vale sisestus! Kontrolli tehte marki! (Lubatud on +, -, *, /)')
+    print('Vale sisestus! Kontrolli tehte marki! (Lubatud on +, -, *, /, ^)')
     print('Kasutusjuhendi nagemiseks lisa skripti kaivitamise kasu loppu sona help.\n')
